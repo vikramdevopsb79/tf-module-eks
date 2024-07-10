1c62e99b411ce7c1dbbdf61d299b15534a264eb2
sai <kuppalasaivikram@gmail.com>
2024-07-10 18:23:20 +0530

    creating autoscaler in terraform to access with service

---
+++ infra-helm-charts.tf
@@ -0,0 +1,41 @@
+resource "null_resource" "get-kubeconfig" {
+
+  depends_on = [aws_eks_node_group.node-groups, aws_eks_cluster.main]
+
+  provisioner "local-exec" {
+    command = <<EOF
+rm -rf ~/.kube
+aws eks update-kubeconfig --name "${var.env}-eks"
+sleep 300
+EOF
+  }
+
+}
+
+data "http" "metric-server" {
+  url = "https://github.com/kubernetes-sigs/metrics-server/releases/latest/download/components.yaml"
+}
+
+data "kubectl_file_documents" "metric-server" {
+  content = data.http.metric-server.body
+}
+
+resource "kubectl_manifest" "metric-server" {
+  depends_on = [null_resource.get-kubeconfig]
+
+  count     = length(data.kubectl_file_documents.metric-server.documents)
+  yaml_body = data.kubectl_file_documents.metric-server.documents[count.index]
+}
+
+## Cluster Autoscaler
+
+data "kubectl_file_documents" "cluster-autoscaler" {
+  content = file("${path.module}/cluster-autoscale-dev.yaml")
+}
+
+resource "kubectl_manifest" "cluster-autoscaler" {
+  depends_on = [null_resource.get-kubeconfig]
+
+  count     = length(data.kubectl_file_documents.cluster-autoscaler.documents)
+  yaml_body = data.kubectl_file_documents.cluster-autoscaler.documents[count.index]
+}
