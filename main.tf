f98c08db3e12ccfd8651598912c6a3342fc2f731
sai <kuppalasaivikram@gmail.com>
2024-07-22 17:01:23 +0530

    domain name changed

--- main.tf
+++ main.tf
@@ -8,6 +8,8 @@ resource "aws_eks_cluster" "main" {
   vpc_config {
     subnet_ids = var.subnet_ids
   }
+  # need to add config permissions for ci -server
+
 }
 
 resource "aws_eks_addon" "vpc-cni" {
@@ -54,3 +56,20 @@ resource "aws_iam_openid_connect_provider" "eks" {
 
   thumbprint_list = [data.external.oidc-thumbprint.result.thumbprint]
 }
+#need to add resources for ci -server
+
+# resource "aws_eks_access_entry" "ci-server" {
+#   cluster_name      = aws_eks_cluster.main.name
+#   principal_arn     = "arn:aws:iam::739561048503:role/ci-server-role"
+#   type              = "STANDARD"
+# }
+#
+# resource "aws_eks_access_policy_association" "ci-server-policy" {
+#   cluster_name  = aws_eks_cluster.main.name
+#   policy_arn    = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
+#   principal_arn = "arn:aws:iam::739561048503:role/ci-server-role"
+#
+#   access_scope {
+#     type       = "cluster"
+#   }
+# }
